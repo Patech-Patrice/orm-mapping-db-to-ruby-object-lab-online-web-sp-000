4e1e2af5c391828091e369cb661d33a1ee77c553
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-08 23:52:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,6 +60,16 @@ class Student
   end
 
 
+  def self.first_X_students_in_grade_10(input)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    LIMIT ?
+    SQL
+    DB[:conn].execute(sql, input) 
+  end
+
 
   def save
     sql = <<-SQL

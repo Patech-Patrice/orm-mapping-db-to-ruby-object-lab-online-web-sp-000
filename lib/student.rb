2ca0e27aca15a4ce2470cb8cf1ea45b12c318d14
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-08 23:49:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,6 @@ class Student
    end
  end
 
-
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
@@ -37,6 +36,17 @@ class Student
      self.new_from_db(row)
    end.first
   end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+
+
 
 
   def save

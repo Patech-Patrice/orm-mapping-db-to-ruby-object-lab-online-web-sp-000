9d68fba8a5f5094b335c5086964262f57ec88801
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-09 00:12:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -84,17 +84,15 @@ class Student
     end.first
   end
 
-  def self.all_students_in_grade_X(input)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE students.grade = 10
-      LIMIT 1
-      SQL
-      DB[:conn].execute(sql).collect do |row|
-        self.new_from_db(row)
-      end.first
-    end
+  def self.all_students_in_grade_X(number)
+   sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE grade = ?
+     SQL
+   DB[:conn].execute(sql, number)
+    end	  
+
 
 
   def save
